2eeb22e896420fe53d0348f652b07e4446295c57
plaban <plaban.dwivedy@hansel.io>
2022-01-31 20:42:12 +0530

    "added driver manager and testng.xml file"

---
+++ src/test/java/Utility/DriverManager.java
@@ -0,0 +1,22 @@
+package Utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class DriverManager {
+      WebDriver driver;
+    public void getDriver(String browser){
+        WebDriverManager.chromedriver().setup();
+        if (browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        }else if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        }else {
+            driver = new InternetExplorerDriver();
+        }
+
+    }
+}
