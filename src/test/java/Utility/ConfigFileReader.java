465357a4e385e0763dfb2c5a8feb98abe68776fb
plaban <plaban.dwivedy@hansel.io>
2022-01-31 20:11:51 +0530

    created project structure

---
+++ src/test/java/Utility/ConfigFileReader.java
@@ -0,0 +1,37 @@
+package Utility;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader {
+    private Properties properties;
+    String propertyFilePath = "src/main/resources/config.properties";
+    public ConfigFileReader() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at ");
+        }
+    }
+    public String getApplicationUrl() {
+        String url = properties.getProperty("url");
+        if(url != null) return url;
+        else throw new RuntimeException("url not specified in the Configuration.properties file.");
+    }
+    public String getSearchedText() {
+        String searchText = properties.getProperty("searchText");
+        if(searchText != null) return searchText;
+        else throw new RuntimeException("searchText not specified in the Configuration.properties file.");
+    }
+}
